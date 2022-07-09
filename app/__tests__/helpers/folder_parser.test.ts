21cb3c9073443c0586e7d7df4059ee8ac797f8fc
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-07-09 17:25:10 -0300

    feat: add tree parser

--- app/__tests__/helpers/folder_parser.test.ts
+++ app/__tests__/helpers/folder_parser.test.ts
@@ -1,4 +1,4 @@
-import { parse } from '../../helpers/folder_utils'
+import { parse, parseFromTree, parseLineTree } from '../../helpers/folder_utils'
 
 describe('folder_utils', () => {
   describe('parse', () => {
@@ -7,34 +7,40 @@ describe('folder_utils', () => {
       const result = parse(text)
       const expected = {
         public: {
+          level: undefined,
           name: 'public',
           icon: 'folder-generic',
           path: ['public'],
           children: {}
         },
         'App.tsx': {
+          level: undefined,
           name: 'App.tsx',
           icon: 'typescript',
           path: ['App.tsx'],
           children: {}
         },
         src: {
+          level: undefined,
           name: 'src',
           icon: 'folder-generic',
           path: ['src'],
           children: {
             assets: {
+              level: undefined,
               name: 'assets',
               icon: 'folder-generic',
               path: ['src', 'assets'],
               children: {
                 images: {
+                  level: undefined,
                   name: 'images',
                   icon: 'folder-generic',
                   path: ['src', 'assets', 'images'],
                   children: {},
                 },
                 svgs: {
+                  level: undefined,
                   name: 'svgs',
                   icon: 'folder-generic',
                   path: ['src', 'assets', 'svgs'],
@@ -70,6 +76,161 @@ describe('folder_utils', () => {
       const text = 'simple text ## test ## other'
       const result = parse(text)
       expect(result).toStrictEqual({})
+    })
+  })
+
+  describe('parseFromTree', () => {
+    const mainExpected = {
+      'README.md': {
+        level: 1,
+        name: 'README.md',
+        icon: 'markdown',
+        path: ['README.md'],
+        children: {}
+      },
+      app: {
+        level: 1,
+        name: 'app',
+        icon: 'folder-generic',
+        path: ['app'],
+        children: {
+          'README.md': {
+            level: 2,
+            name: 'README.md',
+            icon: 'markdown',
+            path: ['app', 'README.md'],
+            children: {}
+          },
+          'assets': {
+            level: 2,
+            name: 'assets',
+            icon: 'folder-generic',
+            path: ['app', 'assets'],
+            children: {
+              'image.png': {
+                level: 3,
+                name: 'image.png',
+                icon: 'folder-generic',
+                path: ['app', 'assets', 'image.png'],
+                children: {}
+              }
+            }
+          },
+          'tailwind.config.js': {
+            level: 2,
+            name: 'tailwind.config.js',
+            icon: 'javascript',
+            path: ['app', 'tailwind.config.js'],
+            children: {}
+          }
+        }
+      }
+    }
+
+    it('parses line', () => {
+      const line1 = '├── README.md'
+      const result1 = parseLineTree(line1)
+      expect(result1).toStrictEqual({ name: 'README.md', level: 1 })
+
+      const line2 = '│   │   ├── fileMock.js'
+      const result2 = parseLineTree(line2)
+      expect(result2).toStrictEqual({ name: 'fileMock.js', level: 3 })
+
+      const line3 = '│   │   └── styleMock.js'
+      const result3 = parseLineTree(line3)
+      expect(result3).toStrictEqual({ name: 'styleMock.js', level: 3 })
+
+      const line4 = '│   │       └── Wrapper.tsx'
+      const result4 = parseLineTree(line4)
+      expect(result4).toStrictEqual({ name: 'Wrapper.tsx', level: 4 })
+    })
+
+    it('should parse tree text without root', () => {
+      const text = `├── README.md
+└── app
+    ├── README.md
+    ├── assets
+    │   └── image.png
+    └── tailwind.config.js`
+      const result = parseFromTree(text)
+      expect(result).toStrictEqual(mainExpected)
+    })
+
+    it('should parse tree text without root and using spaces', () => {
+      const text = `├── README.md
+└── app
+    ├── README.md
+    ├── assets
+    │   └── image.png
+    └── tailwind.config.js`
+      const result = parseFromTree(text)
+      expect(result).toStrictEqual(mainExpected)
+    })
+
+    it('should parse tree text with root', () => {
+      const text = `root
+├── README.md
+└── app
+    ├── README.md
+    ├── assets
+    │   └── image.png
+    └── tailwind.config.js`
+      const result = parseFromTree(text)
+      const expected = {
+        'root': {
+          level: 0,
+          name: 'root',
+          icon: 'folder-generic',
+          path: ['root'],
+          children: {
+            'README.md': {
+              level: 1,
+              name: 'README.md',
+              icon: 'markdown',
+              path: ['root', 'README.md'],
+              children: {}
+            },
+            app: {
+              level: 1,
+              name: 'app',
+              icon: 'folder-generic',
+              path: ['root', 'app'],
+              children: {
+                'README.md': {
+                  level: 2,
+                  name: 'README.md',
+                  icon: 'markdown',
+                  path: ['root', 'app', 'README.md'],
+                  children: {}
+                },
+                'assets': {
+                  level: 2,
+                  name: 'assets',
+                  icon: 'folder-generic',
+                  path: ['root', 'app', 'assets'],
+                  children: {
+                    'image.png': {
+                      level: 3,
+                      name: 'image.png',
+                      icon: 'folder-generic',
+                      path: ['root', 'app', 'assets', 'image.png'],
+                      children: {}
+                    }
+                  }
+                },
+                'tailwind.config.js': {
+                  level: 2,
+                  name: 'tailwind.config.js',
+                  icon: 'javascript',
+                  path: ['root', 'app', 'tailwind.config.js'],
+                  children: {}
+                }
+              }
+            }
+          }
+        }
+      }
+      expect(result).toStrictEqual(expected)
     })
   })
 })
--- app/helpers/folder_utils.ts
+++ app/helpers/folder_utils.ts
@@ -1,8 +1,14 @@
 export interface FolderParseResult {
+  level?: number;
   name: string;
   icon: string;
   path: string[];
   children?: { [key: string]: FolderParseResult };
+}
+
+export interface LineTree {
+  name: string;
+  level: number;
 }
 
 export const FOLDER_SEPARATOR = '/'
@@ -27,8 +33,9 @@ const getIcon = (name: string): string => {
   return mapIcons[key] ?? mapIcons.generic
 }
 
-const buildParseItem = (name: string, path: string[]): FolderParseResult => {
+const buildParseItem = (name: string, path: string[], level?: number): FolderParseResult => {
   return {
+    level,
     name,
     path,
     icon: getIcon(name),
@@ -37,6 +44,12 @@ const buildParseItem = (name: string, path: string[]): FolderParseResult => {
 }
 
 export const parse = (text: string): { [key: string]: FolderParseResult } => {
+  if (!text) return {}
+  const parser = text.includes('├──') ? parseFromTree : parseMarkdown
+  return parser(text)
+}
+
+export const parseMarkdown = (text: string): { [key: string]: FolderParseResult } => {
   const output: { [key: string]: FolderParseResult } = {}
   if (text) {
     const result = text.match(/^(##[^\n#]+)+$/gmi) ?? []
@@ -69,4 +82,54 @@ export const parse = (text: string): { [key: string]: FolderParseResult } => {
   }
 
   return output
+}
+
+export const parseFromTree = (text: string): { [key: string]: FolderParseResult } => {
+  const output: { [key: string]: FolderParseResult } = {}
+  const lines = text.trim().split('\n')
+  const lastLevels = []
+
+  for (let i = 0; i < lines.length; i++) {
+    const rawLine = lines[i]
+    const line = parseLineTree(rawLine)
+    if (line.level === 0) {
+      const item = buildParseItem(line.name, [line.name], line.level)
+      output[item.name] = item
+      lastLevels[line.level] = item
+    } else {
+      const parent = lastLevels[line.level - 1]
+      if (parent) {
+        const path = (parent?.path || []).concat([line.name])
+        const item = buildParseItem(line.name, path, line.level)
+        parent.children[line.name] = item
+        lastLevels[line.level] = item
+      } else {
+        const item = buildParseItem(line.name, [line.name], line.level)
+        output[item.name] = item
+        lastLevels[line.level] = item
+      }
+    }
+  }
+
+  return output
+}
+
+export const parseLineTree = (line: string): LineTree => {
+  const SEPARATOR_ONE = '├──'
+  const SEPARATOR_TWO = '└──'
+  const regex = new RegExp(`(${SEPARATOR_ONE}|${SEPARATOR_TWO})`)
+
+  let name = line
+  let level = 0
+
+  if (regex.test(line)) {
+    const separator = line.includes(SEPARATOR_TWO)
+      ? SEPARATOR_TWO
+      : SEPARATOR_ONE
+    const splits = line.split(separator)
+    level = splits.shift().length / 4 + 1
+    name = splits.shift().trim()
+  }
+
+  return { name, level }
 }
