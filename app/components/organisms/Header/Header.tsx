8ca3c1be6432ec49b398a585da7455eb1b7a555f
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-05 08:09:43 -0300

    chore: add login header

--- app/components/organisms/Header/Header.tsx
+++ app/components/organisms/Header/Header.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useContext } from 'react'
+
+import { UserContext } from '../../../contexts/user_context'
 import { Logo } from '../../molecules/Logo/Logo'
-import { TextContent } from '../TextContent/TextContent'
 
 export interface Props {
   children: React.ReactChild
 }
 
 export const Header: React.FC<Props> = function ({ children }) {
+  const router = useRouter()
+  const { openModal, currentUser } = useContext(UserContext)
+
+  function goProfile() {
+    router.push(`/@${currentUser.username}`)
+  }
+
   return (
     <div className="flex flex-shrink justify-between p-12">
       <div className="flex flex-grow">
         {children}
       </div>
-      <div className="flex flex-shrink-0">
+      <div className="flex flex-shrink-0 gap-4 items-center">
+        <div className="text-white cursor-pointer font-bold bg-white bg-opacity-5 rounded-full px-4 py-2 transition-colors duration-300 hover:bg-opacity-10">
+          {currentUser && (
+            <span onClick={goProfile}>@{currentUser.username}</span>
+          )}
+          {!currentUser && (
+            <span onClick={openModal}>login</span>
+          )}
+        </div>
         <Logo />
       </div>
     </div>
