0c1bf96e031bcd9f3b528826cee3d48afbf17679
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-12 07:48:55 -0300

    chore: add empty structures component

---
+++ app/components/organisms/EmptyStructures/EmptyStructures.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+import { Button } from '../../molecules/Button/Button'
+
+export const EmptyStructures: React.FC = function () {
+  return (
+    <div className="flex flex-col justify-center items-center gap-8 text-center lg:gap-16">
+      <div className="text-3xl lg:text-5xl">You have no structure yet!</div>
+      <div>
+        <Link href="/new">
+          <Button bordered size="large">Create my first!</Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
