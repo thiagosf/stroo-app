755e20556669fae05319fc39a1c9abb8fb0495e7
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-13 07:08:17 -0300

    chore: improve hover component

--- app/components/atoms/HoverUnderlined/HoverUnderlined.tsx
+++ app/components/atoms/HoverUnderlined/HoverUnderlined.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export interface Props {
+  color?: string;
   children?: React.ReactNode;
 }
 
-export const HoverUnderlined: React.FC<Props> = function ({ children }) {
+export const HoverUnderlined: React.FC<Props> = function ({ color, children }) {
+  const borderColor = color ? `border-${color}` : 'border-white'
   return (
-    <div className="hover:underline inline-flex cursor-pointer">
+    <div className={`transition-colors duration-200 border-b-2 border-transparent hover:${borderColor} inline-flex cursor-pointer`}>
       {children}
     </div>
   )
