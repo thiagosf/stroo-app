be74d76f6b782400816bf61541afb62ec52115bc
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-17 14:21:56 -0300

    feat: apply like after login

--- app/hooks/use_favorite.ts
+++ app/hooks/use_favorite.ts
@@ -1,20 +1,24 @@
+import { useContext, useEffect } from 'react'
 import { useMutation } from '@apollo/client'
-import { useContext } from 'react'
-import { SiteContext } from '../contexts/site_context'
 
+import { SiteContext } from '../contexts/site_context'
 import { UserContext } from '../contexts/user_context'
 import { randomEmoji } from '../helpers/emoji'
 import { StructureEntity } from '../pages/[username]/[slug]'
 import { DISLIKE, LIKE } from '../queries/user_like_queries'
 
+import { useLocalStorage } from './use_local_storage'
+
 export function useFavorite() {
   const siteContextValue = useContext(SiteContext)
   const userContextValue = useContext(UserContext)
+  const [lastAction, setLastAction] = useLocalStorage('last_action', null)
   const [like] = useMutation(LIKE)
   const [dislike] = useMutation(DISLIKE)
 
   async function onFavorite(entity: StructureEntity) {
     if (!userContextValue.currentUser) {
+      setLastAction({ action: 'like', value: entity.code })
       userContextValue.openModal()
     } else {
       if (!entity.liked) {
@@ -47,6 +51,28 @@ export function useFavorite() {
       title: error.message
     })
   }
+
+  useEffect(() => {
+    async function applyLastAction() {
+      if (lastAction && lastAction.action === 'like' && userContextValue.currentUser) {
+        like({
+          variables: { code: lastAction.value },
+          onCompleted: (data) => {
+            setLastAction(null)
+            if (siteContextValue.structure.code === lastAction.value) {
+              updateCounter(data.like, true)
+            }
+          },
+          onError(error) {
+            setLastAction(null)
+            onError(error)
+          }
+        })
+      }
+    }
+
+    applyLastAction()
+  }, [userContextValue.currentUser, lastAction])
 
   return [onFavorite]
 }
