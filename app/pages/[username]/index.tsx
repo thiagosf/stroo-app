fb5d647e701255c094ba5add589e8f19b2ed9d2d
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-17 17:27:35 -0300

    chore: use custom suspense in the profile

--- app/pages/[username]/index.tsx
+++ app/pages/[username]/index.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from '@apollo/client'
 import { NextPage } from 'next'
 
-import { Spinner } from '../../components/atoms/Spinner/Spinner'
-import { NotFound } from '../../components/organisms/NotFound/NotFound'
+import { CustomSuspense } from '../../components/organisms/CustomSuspense/CustomSuspense'
 import { Profile } from '../../components/organisms/Profile/Profile'
 import { MainLayout, SeoMeta } from '../../components/templates/MainLayout/MainLayout'
 import { useStructures } from '../../hooks/use_structures'
@@ -26,24 +25,17 @@ const AuthorPage: NextPage<Props> = ({ username }) => {
 
   return (
     <MainLayout seo={seo}>
-      {userLoading && (
-        <div className="flex justify-center items-center grow">
-          <Spinner />
-        </div>
-      )}
-      {!userLoading && !user && (
-        <div className="flex justify-center items-center grow">
-          <NotFound />
-        </div>
-      )}
-      {!userLoading && user && (
+      <CustomSuspense
+        loading={userLoading}
+        notFound={!userLoading && !user}
+      >
         <Profile
           user={user}
           structures={list}
           structuresLoading={structuresLoading}
           loadMoreStructures={loadMoreStructures}
         />
-      )}
+      </CustomSuspense>
     </MainLayout>
   )
 }
