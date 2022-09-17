d77d2e78738e89f8ff4dd44e53d343fe72d36e5b
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-17 14:17:47 -0300

    chore: add size option to spinner

--- app/components/atoms/Spinner/Spinner.tsx
+++ app/components/atoms/Spinner/Spinner.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-export const Spinner: React.FC = function () {
+export type SpinnerSize = 'small'
+
+export interface Props {
+  size?: SpinnerSize;
+}
+
+export const Spinner: React.FC<Props> = function ({ size }) {
+  const boxSize = size === 'small' ? '4' : '10'
+  const borderSize = size === 'small' ? '2' : '4'
+  const circleSize = size === 'small' ? '2' : '6'
+
   return (
-    <div className="inline-flex w-10 h-10 justify-center items-center relative">
-      <div className="border-4 border-transparent border-t-white rounded-full animate-spin-fast absolute top-0 left-0 bottom-0 right-0"></div>
-      <div className="w-6 h-6 border-4 border-green-500 rounded-full"></div>
+    <div className={`inline-flex w-${boxSize} h-${boxSize} justify-center items-center relative`}>
+      <div className={`border-${borderSize} border-transparent border-t-white rounded-full animate-spin-fast absolute top-0 left-0 bottom-0 right-0`}></div>
+      <div className={`w-${circleSize} h-${circleSize} bg-green-500 rounded-full`}></div>
     </div>
   )
 }
