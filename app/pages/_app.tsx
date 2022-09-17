827d3b1105506339ab490d367b92698148177efa
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-17 14:19:37 -0300

    refactor: format

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { ApolloProvider } from '@apollo/client'
-import 'tailwindcss/tailwind.css'
 
-import BaseApp from '../components/templates/BaseApp/BaseApp'
 import { apolloClient } from '../lib/apollo_client'
 import '../styles/globals.css'
+import BaseApp from '../components/templates/BaseApp/BaseApp'
 
 function MyApp(props: any) {
   return (
