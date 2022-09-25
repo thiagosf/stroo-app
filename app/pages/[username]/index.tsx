b650eadfce687d327765953eaf2e0e6e4332226e
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-25 10:32:26 -0300

    chore: improve profile seo

--- app/pages/[username]/index.tsx
+++ app/pages/[username]/index.tsx
@@ -1,48 +1,53 @@
-import { useQuery } from '@apollo/client'
 import { NextPage } from 'next'
 
-import { CustomSuspense } from '../../components/organisms/CustomSuspense/CustomSuspense'
 import { Profile } from '../../components/organisms/Profile/Profile'
 import { MainLayout, SeoMeta } from '../../components/templates/MainLayout/MainLayout'
 import { useStructures } from '../../hooks/use_structures'
+import { apolloClient } from '../../lib/apollo_client'
 import { PUBLIC_PROFILE } from '../../queries/user_queries'
 
 export interface Props {
-  username: string;
+  user: any;
 }
 
-const AuthorPage: NextPage<Props> = ({ username }) => {
-  const { data: userData, loading: userLoading } = useQuery(PUBLIC_PROFILE, {
-    variables: { username }
+const AuthorPage: NextPage<Props> = ({ user }) => {
+  const [list, structuresLoading, loadMoreStructures] = useStructures({
+    username: user.username,
+    limit: 20
   })
-  const [list, structuresLoading, loadMoreStructures] = useStructures({ username, limit: 20 })
 
-  const { profile: user } = userData ?? {}
   const seo: SeoMeta = {
-    title: user ? `@${user.username}` : '...',
-    description: user ? `Structures by @${user.username}` : '...',
+    title: user ? `@${user.username}` : 'User not found',
+    description: user ? `Structures by @${user.username}` : 'User not found',
   }
 
   return (
     <MainLayout seo={seo}>
-      <CustomSuspense
-        loading={userLoading}
-        notFound={!userLoading && !user}
-      >
-        <Profile
-          user={user}
-          structures={list}
-          structuresLoading={structuresLoading}
-          loadMoreStructures={loadMoreStructures}
-        />
-      </CustomSuspense>
+      <Profile
+        user={user}
+        structures={list}
+        structuresLoading={structuresLoading}
+        loadMoreStructures={loadMoreStructures}
+      />
     </MainLayout>
   )
 }
 
-AuthorPage.getInitialProps = function ({ query }) {
+export async function getServerSideProps({ query }) {
   const username = (query.username.toString() ?? '').substring(1)
-  return { username }
+
+  const { data } = await apolloClient.query({
+    query: PUBLIC_PROFILE,
+    variables: { username }
+  })
+  const user = data ? data.profile : null
+  if (!user) {
+    return {
+      notFound: true
+    }
+  }
+  const props = { user }
+  return { props }
 }
 
 export default AuthorPage
