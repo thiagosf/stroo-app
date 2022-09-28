6f056ca27de7a9b6256f707f8aa33100450baec9
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2022-09-28 08:46:08 -0300

    fix: avoid body scroll using flex fixed

--- app/components/atoms/FlexFixed/FlexFixed.tsx
+++ app/components/atoms/FlexFixed/FlexFixed.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react'
+
 interface Props {
   children: React.ReactNode;
 }
 
 export const FlexFixed: React.FC<Props> = ({ children }) => {
+  useEffect(() => {
+    const overflowHidden = 'overflow-hidden'
+    document.body.classList.add(overflowHidden)
+
+    return () => {
+      document.body.classList.remove(overflowHidden)
+    }
+  }, [])
+
   return (
     <div className="flex fixed top-0 left-0 right-0 bottom-0">
       {children}
